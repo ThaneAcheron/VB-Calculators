b1e3107311d017381f0c0d97cab4b6c863bad2ec
ThaneAcheron <craig.turner944@gmail.com>
2018-04-23 09:07:17 +0200

    Final

---
+++ V1/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Label1.Text = "X"
+        Label2.Text = Val(TextBox1.Text) * Val(TextBox2.Text)
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Label1.Text = "%"
+        Label2.Text = Val(TextBox1.Text) / Val(TextBox2.Text)
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Label1.Text = "+"
+        Label2.Text = Val(TextBox1.Text) + Val(TextBox2.Text)
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Label1.Text = "-"
+        Label2.Text = Val(TextBox1.Text) - Val(TextBox2.Text)
+    End Sub
+End Class
---
+++ V2/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,89 @@
+﻿Public Class Form1
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim textbox As String
+        TextBox = TextBox1.Text()
+        TextBox1.Text() = textbox + "2"
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim textbox As String
+        textbox = TextBox1.Text()
+        TextBox1.Text() = textbox + "1"
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Dim textbox As String
+        textbox = TextBox1.Text()
+        TextBox1.Text() = textbox + "3"
+
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Dim textbox As String
+        textbox = TextBox1.Text()
+        TextBox1.Text() = textbox + "4"
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        Dim textbox As String
+        textbox = TextBox1.Text()
+        TextBox1.Text() = textbox + "5"
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        Dim textbox As String
+        textbox = TextBox1.Text()
+        TextBox1.Text() = textbox + "6"
+    End Sub
+
+    Private Sub Button7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button7.Click
+        Dim textbox As String
+        textbox = TextBox1.Text()
+        TextBox1.Text() = textbox + "7"
+    End Sub
+
+    Private Sub Button8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button8.Click
+        Dim textbox As String
+        textbox = TextBox1.Text()
+        TextBox1.Text() = textbox + "8"
+    End Sub
+
+    Private Sub Button9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button9.Click
+        Dim textbox As String
+        textbox = TextBox1.Text()
+        TextBox1.Text() = textbox + "9"
+    End Sub
+
+    Private Sub Button10_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button10.Click
+        Dim textbox As String
+        textbox = TextBox1.Text()
+        TextBox1.Text() = textbox + "0"
+    End Sub
+
+    Private Sub Button11_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button11.Click
+        Dim numb1 As Integer
+        Dim symbol As String
+        numb1 = TextBox1.Text()
+        symbol = "pluss"
+
+    End Sub
+
+    Private Sub Button14_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button14.Click
+
+        Dim numb2 As Integer
+        numb2 = TextBox1.Text()
+        Call addnumbers()
+
+
+
+    End Sub
+
+  
+    Private Sub addnumbers(ByVal numb1 As Integer, ByVal numb2 As Integer)
+
+
+    End Sub
+
+End Class
